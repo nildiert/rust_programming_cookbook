630d5543d1ffa7eb6b504ae31664dc1174cab666
Nildiert Jimenez Jaramillo <niljordan23@gmail.com>
2022-02-04 01:38:54 -0500

    Controlling execution flow

--- execution-flow/src/lib.rs
+++ execution-flow/src/lib.rs
@@ -1,8 +1,87 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn conditionals() {
+        let i = 20;
+        // Rust's if statement does not require parenthesis
+        if i < 2 {
+            assert!(i < 2);
+        } else if i > 2 {
+            assert!(i > 2);
+        } else {
+            assert_eq!(i, 2)
+        }
     }
+
+    #[test]
+    fn more_conditionals(){
+        let my_option = Some(10);
+
+        // If let statements can do simple pattern matching
+        if let Some(unpacked) = my_option {
+            assert_eq!(unpacked, 10);
+        }
+
+        let mut other_option = Some(2);
+        // there is also while let, which does the same thing 
+        while let Some(unpacked) = other_option {
+    
+            // if can also return values in assignments
+            other_option = if unpacked > 0 { 
+                Some(unpacked - 1)
+            } else {
+                None
+            }
+        }
+        assert_eq!(other_option, None)
+    }
+
+
+    #[test]
+    fn loops() {
+
+        let mut i = 42;
+        let mut broke = false;
+
+        // a basic loop with control statements
+        loop {
+            i -= 1;
+            if i < 2 { 
+                broke = true;
+                break;
+            } else if i > 2 { 
+                 continue; 
+            }
+        }
+        assert!(broke);
+
+        // loops and other contructs can be named for better readability ...
+        'outer: loop {
+            'inner: loop {
+                break 'inner; // ... and specifically jumped out of
+            }
+            break 'outer;
+        }
+
+        let mut iterations: u32 = 0;
+
+        let total_squared = loop {
+            iterations += 1;
+
+            if iterations >= 10 {
+                break iterations.pow(2);
+            }
+        };
+        assert_eq!(total_squared, 100);
+
+        for i in 0..10 {
+            assert!(i >= 0 && i < 10 )
+        }
+
+        for v in vec![1, 1, 1, 1].iter() {
+            assert_eq!(v, &1);
+        }
+
+    }
+
 }
---
+++ execution-flow/target/debug/deps/execution_flow-3269d4b246c6c895.d
@@ -0,0 +1,5 @@
+/Users/symplifica/Documents/learning/languages/rust/rust-programming-cookbook/execution-flow/target/debug/deps/execution_flow-3269d4b246c6c895: src/lib.rs
+
+/Users/symplifica/Documents/learning/languages/rust/rust-programming-cookbook/execution-flow/target/debug/deps/execution_flow-3269d4b246c6c895.d: src/lib.rs
+
+src/lib.rs:
